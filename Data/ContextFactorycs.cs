426fd37b82c096a4dfc2adb7c2b85aafc8e3014c
Adrian Hernández Bonilla <adrian_hernandez@ucne.edu.do>
2025-06-12 17:40:50 -0400

    (Esto es algo que yo siempre pongo en mis programas, ayuda a evitar problemas con la migracion)

---
+++ Data/ContextFactorycs.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace DesarrolloDeUnSistemaInformaticoEn_PC.Data
+{
+    public class ContextoFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("workstation id=Proyecto_AplicadoDb.mssql.somee.com;packet size=4096;user id=AHB_SQLLogin_1;pwd=lcv8kpbnpl;data source=Proyecto_AplicadoDb.mssql.somee.com;persist security info=False;initial catalog=Proyecto_AplicadoDb;TrustServerCertificate=True");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
